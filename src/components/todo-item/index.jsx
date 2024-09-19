d810110901c0518fb5a01ee56c63466ff6923912
UsmanTirmizi <usmantirmizi18@gmail.com>
2024-09-19 18:10:12 +0500

    ToDolist Project Completed

---
+++ src/components/todo-item/index.jsx
@@ -0,0 +1,38 @@
+import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
+
+
+
+function TodoItem({todo,fetchDetailsOfCurrentTodo} ){
+    
+    return( 
+    <Card sx={
+        {
+            maxWidth:350,
+            display: "flex",
+            flexDirection:"column",
+            justifyContent:"space-between",
+        }
+    }>
+        <CardContent>
+            <Typography variant="h5" color={'text.secondary'}>
+                {todo?.todo}
+            </Typography>
+        </CardContent>
+        <CardActions>
+            <Button
+            onClick={()=>fetchDetailsOfCurrentTodo(todo?.id)}
+            sx={{
+                backgroundColor:'black',
+                color:'white',
+                opacity: "0.75",
+                '&:hover':{
+                    backgroundColor:'black',
+                    color:'white',
+                    opacity:'1',
+                }
+            }}>Show details</Button>
+        </CardActions>
+    </Card>
+    );
+}
+export default TodoItem;
